c6bc136743e7ea1fa932c33be0bc106774eb5e97
tyanotyrano <tyrano09@gmail.com>
2021-11-06 02:41:47 +0900

    create(junit-level-up project): JUnit 학습 프로젝트 생성

---
+++ src/main/java/com/tyranotyrano/junitlevelup/JunitLevelUpApplication.java
@@ -0,0 +1,13 @@
+package com.tyranotyrano.junitlevelup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitLevelUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitLevelUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyranotyrano/junitlevelup/JunitLevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyranotyrano.junitlevelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitLevelUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

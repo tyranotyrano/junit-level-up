8349cc361008b261041869436a9a008ede429084
tyanotyrano <tyrano09@gmail.com>
2021-11-07 00:41:06 +0900

    test(ParameterizedStudyTest.java): @ParameterizedTest 어노테이션 테스트 코드 작성
    @ParameterizedTest 어노테이션 테스트 코드를 작성한다.
    - ParameterizedStudyTest : @ValueSource, @CvsSource, @NullSource, @EmptySource, @NullAndEmptySource 테스트 코드 작성

---
+++ src/test/java/com/tyranotyrano/study/ParameterizedStudyTest.java
@@ -0,0 +1,71 @@
+package com.tyranotyrano.study;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParameterizedStudyTest {
+
+	@DisplayName("ValueSource int 테스트")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void testValueSource1(int number) {
+		// then
+		assertThat(number).isGreaterThanOrEqualTo(0);
+	}
+
+	@DisplayName("ValueSource int 테스트")
+	@ParameterizedTest
+	@ValueSource(strings = {"a", "b", "c"})
+	void testValueSource2(String str) {
+		// then
+		assertThat(str).isNotBlank();
+	}
+
+	@DisplayName("CsvSource 기본 구분자(,) 테스트")
+	@ParameterizedTest
+	@CsvSource({"1,1", "2,2", "3,3", "4,4"})
+	void testCsvSource1(int n1, int n2) {
+		// then
+		assertEquals(n1, n2);
+	}
+
+	@DisplayName("CsvSource 커스텀 구분자 테스트")
+	@ParameterizedTest
+	@CsvSource(value = {"1:1", "2:2", "3:3", "4:4"}, delimiter = ':')
+	void testCsvSource2(int n1, int n2) {
+		// then
+		assertEquals(n1, n2);
+	}
+
+	@DisplayName("NullSource 테스트")
+	@ParameterizedTest
+	@NullSource
+	void testNullSource(String str) {
+		// then
+		assertNull(str);
+	}
+
+	@DisplayName("EmptySource 테스트")
+	@ParameterizedTest
+	@EmptySource
+	void testEmptySource(String str) {
+		// then
+		assertThat(str).isEmpty();
+	}
+
+	@DisplayName("NullAndEmptySource 테스트")
+	@ParameterizedTest
+	@NullAndEmptySource
+	void testNullAndEmptySource (String str) {
+		// then
+		assertThat(str).isBlank();
+	}
+}

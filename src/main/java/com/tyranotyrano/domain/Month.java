9b370e55f6b1dbc03c22034eebb2e3defe14ef4d
tyanotyrano <tyrano09@gmail.com>
2021-11-15 00:15:27 +0900

    refactor(ParameterizedStudyTest.java): 들여쓰기 Tab -> 4 space 로 변경
    들여쓰기 설정을 Tab -> 4 space 로 변경한다.

--- src/main/java/com/tyranotyrano/domain/Month.java
+++ src/main/java/com/tyranotyrano/domain/Month.java
@@ -1,16 +1,16 @@
 package com.tyranotyrano.domain;
 
 public enum Month {
-	JANUARY,
-	FEBRUARY,
-	MARCH,
-	APRIL,
-	MAY,
-	JUNE,
-	JULY,
-	AUGUST,
-	SEPTEMBER,
-	OCTOBER,
-	NOVEMBER,
-	DECEMBER
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
 }
--- src/test/java/com/tyranotyrano/study/ParameterizedStudyTest.java
+++ src/test/java/com/tyranotyrano/study/ParameterizedStudyTest.java
@@ -22,83 +22,83 @@ import com.tyranotyrano.domain.Month;
 
 public class ParameterizedStudyTest {
 
-	@DisplayName("ValueSource int 테스트")
-	@ParameterizedTest
-	@ValueSource(ints = {1, 2, 3})
-	void testValueSource1(int number) {
-		// then
-		assertThat(number).isGreaterThanOrEqualTo(0);
-	}
+    @DisplayName("ValueSource int 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void testValueSource1(int number) {
+        // then
+        assertThat(number).isGreaterThanOrEqualTo(0);
+    }
 
-	@DisplayName("ValueSource int 테스트")
-	@ParameterizedTest
-	@ValueSource(strings = {"a", "b", "c"})
-	void testValueSource2(String str) {
-		// then
-		assertThat(str).isNotBlank();
-	}
+    @DisplayName("ValueSource int 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "b", "c"})
+    void testValueSource2(String str) {
+        // then
+        assertThat(str).isNotBlank();
+    }
 
-	@DisplayName("CsvSource 기본 구분자(,) 테스트")
-	@ParameterizedTest
-	@CsvSource({"1,1", "2,2", "3,3", "4,4"})
-	void testCsvSource1(int n1, int n2) {
-		// then
-		assertEquals(n1, n2);
-	}
+    @DisplayName("CsvSource 기본 구분자(,) 테스트")
+    @ParameterizedTest
+    @CsvSource({"1,1", "2,2", "3,3", "4,4"})
+    void testCsvSource1(int n1, int n2) {
+        // then
+        assertEquals(n1, n2);
+    }
 
-	@DisplayName("CsvSource 커스텀 구분자 테스트")
-	@ParameterizedTest
-	@CsvSource(value = {"1:1", "2:2", "3:3", "4:4"}, delimiter = ':')
-	void testCsvSource2(int n1, int n2) {
-		// then
-		assertEquals(n1, n2);
-	}
+    @DisplayName("CsvSource 커스텀 구분자 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:2", "3:3", "4:4"}, delimiter = ':')
+    void testCsvSource2(int n1, int n2) {
+        // then
+        assertEquals(n1, n2);
+    }
 
-	@DisplayName("NullSource 테스트")
-	@ParameterizedTest
-	@NullSource
-	void testNullSource(String str) {
-		// then
-		assertNull(str);
-	}
+    @DisplayName("NullSource 테스트")
+    @ParameterizedTest
+    @NullSource
+    void testNullSource(String str) {
+        // then
+        assertNull(str);
+    }
 
-	@DisplayName("EmptySource 테스트")
-	@ParameterizedTest
-	@EmptySource
-	void testEmptySource(String str) {
-		// then
-		assertThat(str).isEmpty();
-	}
+    @DisplayName("EmptySource 테스트")
+    @ParameterizedTest
+    @EmptySource
+    void testEmptySource(String str) {
+        // then
+        assertThat(str).isEmpty();
+    }
 
-	@DisplayName("NullAndEmptySource 테스트")
-	@ParameterizedTest
-	@NullAndEmptySource
-	void testNullAndEmptySource (String str) {
-		// then
-		assertThat(str).isBlank();
-	}
+    @DisplayName("NullAndEmptySource 테스트")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void testNullAndEmptySource (String str) {
+        // then
+        assertThat(str).isBlank();
+    }
 
-	@DisplayName("EnumSource 테스트")
-	@ParameterizedTest
-	@EnumSource(value = Month.class)
-	void testEnumSource(Month month) {
-		// then
-		assertTrue(Arrays.asList(Month.values()).contains(month));
-	}
+    @DisplayName("EnumSource 테스트")
+    @ParameterizedTest
+    @EnumSource(value = Month.class)
+    void testEnumSource(Month month) {
+        // then
+        assertTrue(Arrays.asList(Month.values()).contains(month));
+    }
 
-	@DisplayName("MethodSource 테스트")
-	@ParameterizedTest
-	@MethodSource(value = "provideStringsForIsBlank")
-	void testMethodSource(String input, boolean expected) {
-		assertEquals(expected, Strings.isBlank(input));
-	}
+    @DisplayName("MethodSource 테스트")
+    @ParameterizedTest
+    @MethodSource(value = "provideStringsForIsBlank")
+    void testMethodSource(String input, boolean expected) {
+        assertEquals(expected, Strings.isBlank(input));
+    }
 
-	private static Stream<Arguments> provideStringsForIsBlank() {
-		return Stream.of(
-			Arguments.of(null, true),
-			Arguments.of("", true),
-			Arguments.of("  ", true),
-			Arguments.of("not blank", false)
-		);
-	}
+    private static Stream<Arguments> provideStringsForIsBlank() {
+        return Stream.of(
+            Arguments.of(null, true),
+            Arguments.of("", true),
+            Arguments.of("  ", true),
+            Arguments.of("not blank", false)
+        );
+    }
 }

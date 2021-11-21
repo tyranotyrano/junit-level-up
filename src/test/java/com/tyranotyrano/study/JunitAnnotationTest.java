216c731fa35c9b8896ed80bff5d6a466b6a3eeb4
tyanotyrano <tyrano09@gmail.com>
2021-11-21 22:08:15 +0900

    test(JunitAnnotationTest.java): @beforeEach 기본 테스트 코드 추가
    @beforeEach 기본 테스트 코드를 추가한다.
    - JunitAnnotationTest : @beforeEach 기본 테스트 코드 추가

--- src/test/java/com/tyranotyrano/study/JunitAnnotationTest.java
+++ src/test/java/com/tyranotyrano/study/JunitAnnotationTest.java
@@ -2,7 +2,6 @@ package com.tyranotyrano.study;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,5 +22,25 @@ public class JunitAnnotationTest {
     void testBeforeEach() {
         // then
         assertThat(b).isGreaterThanOrEqualTo(a);
+    }
+
+    @DisplayName("@beforeEach 테스트2")
+    @Test
+    void testBeforeEach2() {
+        // given
+        a = 2;
+
+        // when & then
+        assertThat(b).isEqualTo(a);
+    }
+
+    @DisplayName("@beforeEach 테스트3")
+    @Test
+    void testBeforeEach3() {
+        // given
+        a = 3;
+
+        // when & then
+        assertThat(b).isLessThanOrEqualTo(a);
     }
 }

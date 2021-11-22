9c43bc55463385cefae0640750d7f18cf39a223e
tyanotyrano <tyrano09@gmail.com>
2021-11-22 22:12:17 +0900

    test(AssertionsTest.java): isEqualTo() 기본 테스트 코드 추가
    isEqualTo() 기본 테스트 코드를 추가한다.
    - JunitAnnotationTest : isEqualTo() 기본 테스트 코드 추가

--- src/test/java/com/tyranotyrano/study/AssertionsTest.java
+++ src/test/java/com/tyranotyrano/study/AssertionsTest.java
@@ -1,9 +1,22 @@
 package com.tyranotyrano.study;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AssertionsTest {
 
+    @DisplayName("isEqualTo 테스트")
+    @Test
+    void testIsEqualTo() {
+        // given
+        int expected = 1;
 
+        // when
+        int actual = 1;
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
 }

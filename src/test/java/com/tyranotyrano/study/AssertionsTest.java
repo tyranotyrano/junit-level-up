a2f0b2c5e6eedcd5cfcdc9bbe384029bfffbe5ca
tyanotyrano <tyrano09@gmail.com>
2021-11-23 23:09:40 +0900

    test(AssertionsTest.java): isLess/GreaterThanOrEqualTo() 기본 테스트 코드 추가
    isLess/GreaterThanOrEqualTo() 기본 테스트 코드를 추가한다.
    - JunitAnnotationTest : isLess/GreaterThanOrEqualTo() 기본 테스트 코드 추가

--- src/test/java/com/tyranotyrano/study/AssertionsTest.java
+++ src/test/java/com/tyranotyrano/study/AssertionsTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class AssertionsTest {
+class AssertionsTest {
 
     @DisplayName("isEqualTo 테스트")
     @Test
@@ -18,5 +18,31 @@ public class AssertionsTest {
 
         // then
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("isLessThanOrEqualTo 테스트")
+    @Test
+    void testIsLessThanOrEqualTo() {
+        // given
+        int expected = 2;
+
+        // when
+        int actual = 1;
+
+        // then
+        assertThat(actual).isLessThanOrEqualTo(expected);
+    }
+
+    @DisplayName("isGreaterThanOrEqualTo 테스트")
+    @Test
+    void testIsGreaterThanOrEqualTo() {
+        // given
+        int expected = 1;
+
+        // when
+        int actual = 2;
+
+        // then
+        assertThat(actual).isGreaterThanOrEqualTo(expected);
     }
 }
